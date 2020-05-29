5653ed98ff3ed731620cd1682dddac7c61139cd0
Koliqa <kolyaleonof@yandex.ru>
2020-05-29 21:55:51 +0300

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+
+    name = 'mymod1v1KLQ',
+    version = '0.0',
+    description = 'None discription',
+    packages = ['mymod1v1KLQ'],
+    author_email = 'kolyaleonof@yandex.ru',
+    zip_safe = False
+    
+    )
